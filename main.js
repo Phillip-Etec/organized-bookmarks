1ac0b43e3cf7ad720c5c9532915d3e2dade4901a
Phillip-Etec <phillip.anselmo.matos@gmail.com>
2023-10-25 00:07:24 -0300

    tudo feito nas 2 ultimas aulas

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, Notification } = require("electron");
 const path = require("path");
 
 const loadMainWindow = () => {
